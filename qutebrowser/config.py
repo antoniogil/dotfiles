44ca00ff2e971fe34efabd7a2d6bd536f20a3c0a
= <antonio.gil.fierro@gmail.com>
2024-11-04 19:14:28 +0100

    added .gitignore. added qutebrowser/dracula

---
+++ qutebrowser/config.py
@@ -0,0 +1,12 @@
+import dracula.draw
+
+c.completion.shrink = True
+
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
