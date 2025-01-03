898c0889c93352e0af1eb5d9f42473db0d746a1d
Antonio Gil Fierro <antonio.gil.fierro@gmail.com>
2025-01-03 08:00:59 +0100

    modified rofi. modified waybar. added .gitignore

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,13 +3,3 @@
 -- Add any additional options here
 
 vim.opt.relativenumber = false
-
-require("telescope").setup({
-  defaults = { file_ignore_patterns = {
-    "node_modules",
-    "bin",
-    "obj",
-    ".next",
-    ".git",
-  } },
-})
