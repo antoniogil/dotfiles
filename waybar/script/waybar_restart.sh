70bb6bcbd50f5c847e2e61411d2776ff53c96875
Antonio Gil <antonio.gil.fierro@gmail.com>
2026-04-12 18:16:18 +0200

    another changes

--- waybar/script/waybar_restart.sh
+++ waybar/script/waybar_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if pgrep -x "waybar" >/dev/null; then
-    killall waybar
+  killall waybar
 fi
 
-waybar &
+waybar -c ~/.config/waybar/config_horizontal -s ~/.config/waybar/stylehor.css &
